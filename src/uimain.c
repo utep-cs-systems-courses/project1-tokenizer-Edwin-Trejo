0a605f649db28356061ab13ea3878491f4581a11
Edwin Trejo <etrejo9@miners.utep.edu>
2020-09-20 21:49:22 -0600

    uimain.c added

---
+++ src/uimain.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Welcome!\n");
+
+
+  char str[30];
+  printf("Enter a string:\n");
+  gets(str);
+
+  printf("You entered:\n");
+  puts(str);
+
+
+
+
+
+
+ done:
+  return 0;
+}
