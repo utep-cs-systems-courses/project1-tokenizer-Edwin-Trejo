a42c5c099429e2d38238110a9d4f71d2027e434b
Edwin-Trejo <etrejo9@miners.utep.edu>
2020-09-27 21:45:40 -0600

    added print_history to history.c

--- src/history.c
+++ src/history.c
@@ -5,6 +5,7 @@ List* init_history(){
   struct List* new_list = NULL;
 }
 
+/*adds a new node to the linked list, if the linked list is empty the node added will be assigned as the root, if not, the while loop will continue until the next node is NULL, then it will insert the next node there */
 void add_history(List *list, char*str){
   Item *head = list->root;
   Item *last = head;
@@ -21,8 +22,20 @@ void add_history(List *list, char*str){
   last->next = new_item;
 
   return;
-  
+}
 
-  
+char *get_history(List *list, int id){
+  return 0;
+}
 
+/*This functions goes through the linked list printing each of the items data as it goes through all of them*/
+void print_history(List *list){
+  Item *current = list->root;
+  while(current != NULL){
+    printf("%s ", current->str);
+    current = current->next;
+  }
+}
+
+void free_history(List *list){
 }
