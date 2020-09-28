2fc6586ba64f3e520d87917749ae5af6c9afcc4c
Edwin-Trejo <etrejo9@miners.utep.edu>
2020-09-27 20:27:49 -0600

    Finished print and free funcitons on tokenizer.c, added descriptions for each function

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,6 +1,6 @@
 #include "malloc.h"
 #include "tokenizer.h"
-
+/*this function looks for a space char ' ' */
 int space_char(char c)
 {
   if(c == '\t' || c == ' '){
@@ -9,6 +9,7 @@ int space_char(char c)
   return 0;
 }
 
+/*This function looks for a non space char*/
 int non_space_char(char c)
 {
   if(c == '\t' || c == ' '){
@@ -16,7 +17,7 @@ int non_space_char(char c)
   }
   return 1;
 }
-
+/*This function uses a for loop to go through the string looking for a certain condition in order to determin if that char is the beginning of a word*/
 char *word_start(char *str)
 {
   char *ptr;
@@ -28,9 +29,9 @@ char *word_start(char *str)
   }
   return 0;
 }
+/*just as word_Starter, this function compares each character in order to find the last char of a word */
 
 char *word_terminator(char *word){
-  char *str = word_start(word);
   char *ptr;
   for (int i =0; word[i] != '\t'; i++) {
     if(word[i] != ' ' && word[i+1] == ' ' ){
@@ -40,6 +41,7 @@ char *word_terminator(char *word){
   }
   return 0;
 }
+/*It goes through the whole string, char by char looking for spaces ' ', when it find one and it is not the end of the string it adds one word to the count */
 
 int count_words(char *str){
   int count = 0;
@@ -53,6 +55,7 @@ int count_words(char *str){
   }
   return count+1;
 }
+/*First it allocates memory by using malloc, and the size of len, then the for loop copies the string, or in this case the array into the new one and returns it */
 
 char *copy_str(char *inStr, short len){
   char *ptr;
@@ -72,11 +75,19 @@ char **tokenize(char* str){
   
   return 0;
 }
+/*This function uses a for loop to go through **tokens, and then print each word as it goes */
 
 void print_tokens(char **tokens){
+  for (int i = 0; *tokens !=""; i++) {
+    printf("%s\n",*tokens[i]); 
+  }
 }
 
+/*similar to print_tokens, this function uses a for loop to go through tokens and free the memory on them*/
 void free_tokens(char **tokens){
+  for (int i = 0; *tokens !=""; i++) {
+    free(tokens[i]);
+  }
 }
   
   
