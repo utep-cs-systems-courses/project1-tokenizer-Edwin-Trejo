2a63daaf8d52b82133027b298fa264c2b9d94f57
Edwin-Trejo <etrejo9@miners.utep.edu>
2020-09-27 21:38:00 -0600

    added add history function

--- src/history.c
+++ src/history.c
@@ -2,13 +2,27 @@
 #include "history.h"
 
 List* init_history(){
-  
+  struct List* new_list = NULL;
 }
 
 void add_history(List *list, char*str){
-  List *current = list;
-  //List last = list->root;
-  List *new_item = (List*) malloc(sizeof(List));
+  Item *head = list->root;
+  Item *last = head;
+  Item *new_item = (Item*) malloc(sizeof(List));
+  new_item->str = str;
+  new_item->next = NULL;
 
+  if(head == NULL){
+    head = new_item;
+  }
+  while(last->next != NULL){
+    last = last->next;
+  }
+  last->next = new_item;
+
+  return;
+  
+
+  
 
 }
